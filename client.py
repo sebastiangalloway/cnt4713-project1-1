90860e6326d1054a9910d07eb4f95a1760fc86f8
sebastiangalloway <sgalloway111@gmail.com>
2024-02-04 21:29:23 -0500

    send

--- client.py
+++ client.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
 
 import sys
+import socket
 
+def send(sock, msg):
+    total_sent = 0
+    while total_sent < len(msg):
+        try:
+            sent = sock.send(msg[total_sent:])
+            if sent == 0:
+                raise sock.error("ERROR: Connection broken")
+            total_sent += sent
+        except sock.error as e:
+            sys.stderr.write("ERROR: sending data\n")
+            sys.exit(1)
+
+
+  
 if __name__ == '__main__':
-    sys.stderr.write("client is not implemented yet\n")
+    main()
