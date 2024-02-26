aa00936df9287bbe6f958e8a34da957115efa759
sebastiangalloway <113319265+sebastiangalloway@users.noreply.github.com>
2024-02-25 19:34:49 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,64 +1,86 @@
-#!/usr/bin/env python3
+import socket
+import sys
+import signal
+import time
 
-import socket, sys
+# define signal handler function
+def signal_handler(signal, frame):
+    global not_stopped
+    print('Exiting gracefully...')
+    not_stopped = False
+    sys.exit(0) # exit with code 0
 
-class Server:
-    def __init__(self, sock = None, secs = 10):
-        if sock is None:
-            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        else:
-            self.sock = sock
-        
-        self.sock.settimeout(secs)
-    
-    def connect(self, port, host = "0.0.0.0"):
-        try:
-            self.sock.bind((host, port))
-            self.sock.listen(10)
-        except socket.error as msg:
-            self.sock.close()
-            sys.stderr.write("ERROR: %s\n" % msg)
-            sys.exit(1)
+# register signal handlers
+signal.signal(signal.SIGQUIT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
+signal.signal(signal.SIGINT, signal_handler)
 
-        connection, address = self.sock.accept()
-        data = ""
-                
-        with connection:
-            try:
-                data += connection.recv(4096)
-                print(data)
-                connection.send(b"Successfully connected to server")
-            except socket.error as msg:
-                self.sock.close()
-                connection.close()
-                sys.stderr.write("ERROR: %s\n" % msg)
-                sys.exit(1)
-            finally:
-                self.sock.close()
-                connection.close()
-                
-def main():
-    # Command line arguments format:
-    # python3 server.py <PORT> <FILE-DIR>
-    
-    if len(sys.argv) != 3:
-        sys.stderr.write("ERROR: Invalid number of arguments\n")
-        sys.exit(1)
-    
-    try:
-        port = int(sys.argv[1])
-    except ValueError:
-        sys.stderr.write("ERROR: Non-integer port number\n")
-        sys.exit(1)
-        
-    if port < 1 or port > 65535:
-        sys.stderr.write("ERROR: Invalid port number\n")
-        sys.exit(1)
-        
-    file_directory = sys.argv[2]
-    
-    s = Server()
-    s.connect(port)
-    
+# parse command line arguments
+if len(sys.argv) < 2:
+    sys.stderr.write("ERROR: Missing port number\n")
+    sys.exit(1)
+
+try:
+    port = int(sys.argv[1])
+
+except ValueError:
+    sys.stderr.write("ERROR: Invalid port number\n")
+    sys.exit(1)
+
+if port > 65535:
+    sys.stderr.write("ERROR: Invalid port number\n")
+    sys.exit(1)
+
+if port < 0:
+    sys.stderr.write("ERROR: Invalid port number\n")
+    sys.exit(1)
+
+
+
+def server_program():
+    # get the hostname
+    host = socket.gethostname()
+    port = int(sys.argv[1])  # initiate port no above 1024
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # get instance
+    # look closely. The bind() function takes tuple as argument
+    server_socket.bind((host, port))  # bind host address and port together
+
+    # configure how many client the server can listen simultaneously
+    server_socket.listen(10)
+    client_socket, server_address = server_socket.accept()  # accept new connection
+
+    client_socket.settimeout(10)
+
+        # send accio command to the client
+    #conn.send(b'accio\r\n')
+
+
+    #print("Connection from: " + str(address))
+    while True:
+        # receive data stream. it won't accept data packet greater than 1024 bytes
+        data = client_socket.recv(1024)#.decode()
+        if not data:
+            # if data is not received break
+            break
+        #print("from connected user: " + str(data))
+        #data = input(' -> ')
+
+    client_socket.send(b'accio\r\n')
+    while True:
+        # receive data stream. it won't accept data packet greater than 1024 bytes
+        data = client_socket.recv(1024)#.decode()
+        if not data:
+            # if data is not received break
+            break
+        #print("from connected user: " + str(data))
+        #data = input(' -> ')
+
+    client_socket.send(b'accio\r\n')
+        #conn.send(data.encode())  # send data to the client
+
+    client_socket.close()  # close the connection
+
+
 if __name__ == '__main__':
-    main()
+    server_program()
