a4b4d6995d2616af61b83465f374e68d7b64f88f
sebastiangalloway <113319265+sebastiangalloway@users.noreply.github.com>
2024-02-25 19:16:26 -0500

    Update server-s.py

--- server-s.py
+++ server-s.py
@@ -1,90 +1,32 @@
 import socket, sys
-        
-def send(socket, msg):
-    total_sent = 0
-    while total_sent < len(msg):
-        sent = socket.send(msg[total_sent:])
-        if sent == 0:
-            raise RuntimeError("ERROR: Socket connection broken\n")
-        total_sent += sent
-    #print("Sent:", msg)
-    #print("Total sent:", total_sent)
 
-def receive(socket, str):
-    msg = b""
-    while str not in msg:
-        chunk = socket.recv(4096)
-        if not chunk:
-            break
-        msg += chunk
-    #print("Received:", msg)
-    return msg
-
-def connect(port, host = "0.0.0.0"):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        sock.bind((host, port))
-        
-    except socket.error as msg:
-        sock.close()
-        sys.stderr.write("ERROR: Connection failed")
-        sys.exit(1)
-        
-    while True:        
-        try:
-            sock.settimeout(10)
-            sock.listen(10)
-            connection, address = sock.accept()
-            connection.settimeout(10)
-                        
-        except socket.error as msg:
+host = '0.0.0.0'
+port = sys.argv[1]
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print(sock)
+if int(port) < 0 and int(port) < 65535:
+    sys.stderr.write("ERROR: port number is out off range 0-65535\n")
+    exit(1)
+else:   
+    sock.bind((host, int(port)))
+    sock.listen(10)
+    clientSocket, clientAddress = sock.accept()
+    clientSocket, clientAddress = sock.accept()
+    sock.setblocking(False)
+    sock.send("accio\r\n")
+    if sock.recv(1024) == b'confirm-accio\r\n':
+        sock.send("accio")
+        if sock.recv(1024) == b'confirm-accio\r\n\r\n':
+            clientSocket, clientAddress = sock.accept()
+            print("Accepted connection from", clientAddress)
+            sock.setblocking(False)
+            data = clientSocket.recv(1024)
+            if data:
+                print("received bytes:", len(data))
+                len = clientSocket.send(data)
+                print("send bytes: %d" % len)
             sock.close()
-            sys.stderr.write("ERROR: %s\n" % msg)
-            sys.exit(1)
-        
-        send(connection, b"accio\r\n")
-        msg = receive(connection, b"confirm-accio\r\n")
-        
-        if msg == b"confirm-accio\r\n":
-            send(connection, b"accio\r\n")
-            msg = receive(connection, b"\r\n")
-            
-            if msg == b"confirm-accio-again\r\n":
-                msg = receive(connection, b"\r\n")                                                                                            
-                sock.close()
-                connection.close()
-                sys.exit(0)
-            else:
-                sock.close()
-                connection.close()
-                sys.stderr.write("ERROR: Invalid data\n")
-                sys.exit(1)
-
         else:
-            sock.close()
-            connection.close()
-            sys.stderr.write("ERROR: Invalid data\n")
-            sys.exit(1)
-
-def main():
-    # Command line arguments format:
-    # python3 server-s.py <PORT>
-    
-    if len(sys.argv) != 2:
-        sys.stderr.write("ERROR: Invalid number of arguments\n")
-        sys.exit(1)
-    
-    try:
-        port = int(sys.argv[1])
-    except ValueError:
-        sys.stderr.write("ERROR: Non-integer port number\n")
-        sys.exit(1)
-        
-    if port < 1 or port > 65535:
-        sys.stderr.write("ERROR: Invalid port number\n")
-        sys.exit(1)
-
-    connect(port)
-    
-if __name__ == '__main__':
-    main()
+            exit(1)
+    else:
+        exit(1)
